696e1eda499711793751b9bd4b7c57bf8ef0425b
yxxdz <20142551@sungshin.ac.kr>
2022-03-23 03:04:57 +0900

    RedirectAttributes 추가

--- src/main/java/jpabook/jpashop/user/controller/UsrOrderController.java
+++ src/main/java/jpabook/jpashop/user/controller/UsrOrderController.java
@@ -8,8 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,10 +48,33 @@ public class UsrOrderController {
     @PostMapping("/order")
     public String order(@RequestParam("memberId") Long memberId,
                         @RequestParam("itemId") Long itemId,
-                        @RequestParam("count") int count) {
+                        @RequestParam("count") int count,
+                        RedirectAttributes redirectAttributes) {
 
         orderService.order(memberId, itemId, count);
-        return "redirect:/";
+
+        String str = String.valueOf(memberId);
+        redirectAttributes.addFlashAttribute("memberId", str);
+        return "redirect:/orders";
+    }
+
+    // 회원 주문 목록
+    @RequestMapping("/orders")
+    public String redirectOrderList(HttpServletRequest request, Model model) {
+
+        String str = "";
+
+        Map<String, ?> redirectMap = RequestContextUtils.getInputFlashMap(request);
+        if (redirectMap != null) {
+            str = (String) redirectMap.get("memberId");
+        }
+
+        Long memberId = Long.parseLong(str);
+
+        List<Order> orders = orderService.findOrders(memberId);
+        model.addAttribute("orders", orders);
+
+        return "user/order/usrOrderList";
     }
 
     // 회원 주문 목록
@@ -62,8 +89,14 @@ public class UsrOrderController {
     }
 
     @PostMapping("/orders/{orderId}/cancel")
-    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+    public String cancelOrder(@PathVariable("orderId") Long orderId,
+                              @RequestParam("memberId") Long memberId,
+                              RedirectAttributes redirectAttributes) {
+
         orderService.cancelOrder(orderId);
+
+        String str = String.valueOf(memberId);
+        redirectAttributes.addFlashAttribute("memberId", str);
         return "redirect:/orders";
     }
 }
