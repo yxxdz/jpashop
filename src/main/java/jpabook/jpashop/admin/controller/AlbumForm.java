697d106c0404e3b28720704d0b0ebf007cfafa48
yxxdz <20142551@sungshin.ac.kr>
2022-03-23 21:04:08 +0900

    Form 클래스 추가

---
+++ src/main/java/jpabook/jpashop/admin/controller/AlbumForm.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.admin.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AlbumForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String artist;
+    private String etc;
+}
---
+++ src/main/java/jpabook/jpashop/admin/controller/MovieForm.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.admin.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MovieForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String director;
+    private String actor;
+}
